1c24a0dbd11d01531a17094ea08f19621dfb368b
Lucas Ramos <lucas.ramos@seati.ma.gov.br>
2022-11-12 07:49:18 -0300

    NLW Copa Dia 2

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            email: 'johndoe@gmail.com',
+            avatarUrl: 'https://github.com/lukeversus.png'
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data: {
+            title: 'Example Pool',
+            code: 'BOL123',
+            ownerId: user.id,
+
+            participants: {
+                create: {
+                    userId: user.id
+                }
+            }
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-04T14:00:00.000Z',
+            firstTeamCountryCode: 'DE',
+            secondTeamCountryCode: 'BR'
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-04T14:00:00.000Z',
+            firstTeamCountryCode: 'BR',
+            secondTeamCountryCode: 'AR',
+
+            guesses: {
+                create: {
+                    firstTeamScore: 2,
+                    secondTeamScore: 1,
+                    participant: {
+                        connect: {
+                            userId_poolId: {
+                                userId: user.id,
+                                poolId: pool.id
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+}
+
+main()
